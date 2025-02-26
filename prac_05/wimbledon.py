1540a342c3f3679e9ba9dfbde42172a34c40a00d
katielin <kyaesinmoehtet@my.jcu.edu.au>
2025-02-26 16:18:44 +0800

    Initialize data structures for champion wins and countries

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -5,3 +5,5 @@ def main():
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         reader = csv.reader(in_file)
         next(reader)  # Skip header
+        champion_wins = {}
+        countries = set()
