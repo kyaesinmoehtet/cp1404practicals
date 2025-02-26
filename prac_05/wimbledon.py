6473195944eb6308d31409961385f3ad27bf4567
katielin <kyaesinmoehtet@my.jcu.edu.au>
2025-02-26 16:17:47 +0800

    Define main function and open file

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,7 @@
+import csv
+
+def main():
+    filename = "wimbledon.csv"
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        reader = csv.reader(in_file)
+        next(reader)  # Skip header
